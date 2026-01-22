17454023c8ceaa7f3874fb4403326f75c2c64376
Duong <duong24072005@gmail.com>
2026-01-22 19:29:55 +0700

    hoan thanh demo 2

---
+++ Assets/scripts/planeshoot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlaneShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))   // chuột trái
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
---
+++ Assets/scripts/shooter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    public float lifeTime = 3f;
+
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+
+        Destroy(gameObject, lifeTime);
+    }
+}
